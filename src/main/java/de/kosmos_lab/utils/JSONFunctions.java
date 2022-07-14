0ea101c70fb999e734649d6f26adbd7c979fdb76
Jan Janssen <jan.janssen@dfki.de>
2022-07-14 17:37:37 +0200

    * updated to 0.1.7 * added JSONArray functionality to pathing in JSONFunctions

--- src/main/java/de/kosmos_lab/utils/JSONFunctions.java
+++ src/main/java/de/kosmos_lab/utils/JSONFunctions.java
@@ -6,6 +6,8 @@ import org.json.JSONObject;
 
 import javax.annotation.CheckForNull;
 import java.util.HashSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class JSONFunctions {
     public static boolean hasEntries(JSONArray jsonArray, String[] keys) {
@@ -15,30 +17,34 @@ public class JSONFunctions {
                 set.add(jsonArray.getString(i));
             }
         } catch (Exception ex) {
-            
+
             return false;
         }
         for (String k : keys) {
             if (!set.contains(k)) {
-                
+
                 return false;
-                
+
             }
         }
-        
+
         return true;
     }
+
     public static boolean hasEntries(JSONObject json, String[] keys) {
-        
+
         for (String k : keys) {
             if (!json.has(k)) {
-                
+
                 return false;
             }
         }
         return true;
     }
-        public static Object get(@CheckForNull JSONObject json, @CheckForNull String path) {
+
+    final static Pattern jsonArrayPattern = Pattern.compile("(.*?)\\[(\\d*?)\\](.*)");
+
+    public static Object get(@CheckForNull JSONObject json, @CheckForNull String path) {
         if (path == null || json == null) {
             return false;
         }
@@ -47,20 +53,58 @@ public class JSONFunctions {
         }
         int idx = path.indexOf("/");
         if (idx == -1) {
-            try {
-                return json.get(path);
-            } catch (JSONException e) {
-            
+            Matcher m = jsonArrayPattern.matcher(path);
+            if ( m.matches()) {
+                JSONArray a = json.optJSONArray(m.group(1));
+                if ( a != null ) {
+                    try {
+                        return a.get(Integer.parseInt(m.group(2)));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+            else {
+                try {
+                    return json.get(path);
+                } catch (JSONException e) {
+
+                }
             }
             return null;
         }
         String p = path.substring(0, idx);
+        Matcher m = jsonArrayPattern.matcher(path);
+        if ( m.matches()) {
+
+            JSONArray a = json.optJSONArray(m.group(1));
+            if ( a != null ) {
+                try {
+
+                    Object o = a.get(Integer.parseInt(m.group(2)));
+                    if ( o instanceof JSONObject) {
+                        if ( m.groupCount()>2) {
+
+                            return get((JSONObject) o, m.group(3));
+                        }
+                        return get((JSONObject) o, null);
+                    }
+                    return o;
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
         if (json.has(p)) {
             return get(json.getJSONObject(p), path.substring(idx + 1));
         }
         return null;
     }
-    
+
     public static boolean has(@CheckForNull JSONObject json, @CheckForNull String path) {
         if (path == null || json == null) {
             return false;
@@ -78,6 +122,6 @@ public class JSONFunctions {
         }
         return false;
     }
-    
-    
+
+
 }
--- src/test/java/JSONFunctionTests.java
+++ src/test/java/JSONFunctionTests.java
@@ -1,5 +1,6 @@
 
 import de.kosmos_lab.utils.JSONFunctions;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -8,44 +9,60 @@ public class JSONFunctionTests {
     @Test
     public void testHasWithPathing() {
         JSONObject json = new JSONObject();
-        
-        Assert.assertFalse(JSONFunctions.has(json,"/test"));
-        json.put("test","1");
-        Assert.assertTrue(JSONFunctions.has(json,"/test"));
-        Assert.assertTrue(JSONFunctions.has(json,"test"));
-        Assert.assertFalse(JSONFunctions.has(json,"test2"));
-        json.put("test",new JSONObject().put("test2",1));
-        Assert.assertTrue(JSONFunctions.has(json,"/test/test2"));
-        Assert.assertFalse(JSONFunctions.has(json,"/test/test"));
-        Assert.assertTrue(JSONFunctions.has(json,"///test///test2"));
-    
+
+        Assert.assertFalse(JSONFunctions.has(json, "/test"));
+        json.put("test", "1");
+        Assert.assertTrue(JSONFunctions.has(json, "/test"));
+        Assert.assertTrue(JSONFunctions.has(json, "test"));
+        Assert.assertFalse(JSONFunctions.has(json, "test2"));
+        json.put("test", new JSONObject().put("test2", 1));
+        Assert.assertTrue(JSONFunctions.has(json, "/test/test2"));
+        Assert.assertFalse(JSONFunctions.has(json, "/test/test"));
+        Assert.assertTrue(JSONFunctions.has(json, "///test///test2"));
+
     }
-    
-    
+
+
     @Test
     public void testGetWithPathing() {
         JSONObject json = new JSONObject();
-        
-        Object obj = JSONFunctions.get(json,"/test");
+
+        Object obj = JSONFunctions.get(json, "/test");
         Assert.assertNull(obj);
-        json.put("test","1");
-        obj = JSONFunctions.get(json,"/test");
+        json.put("test", "1");
+        obj = JSONFunctions.get(json, "/test");
         Assert.assertNotNull(obj);
-        Assert.assertEquals(obj,"1");
-        obj = JSONFunctions.get(json,"test");
+        Assert.assertEquals(obj, "1");
+        obj = JSONFunctions.get(json, "test");
         Assert.assertNotNull(obj);
-        Assert.assertEquals(obj,"1");
-        obj = JSONFunctions.get(json,"test2");
+        Assert.assertEquals(obj, "1");
+        obj = JSONFunctions.get(json, "test2");
         Assert.assertNull(obj);
-        json.put("test",new JSONObject().put("test2",1));
-        obj = JSONFunctions.get(json,"/test/test2");
+        json.put("test", new JSONObject().put("test2", 1));
+        obj = JSONFunctions.get(json, "/test/test2");
         Assert.assertNotNull(obj);
-        obj = JSONFunctions.get(json,"/test/test");
+        obj = JSONFunctions.get(json, "/test/test");
         Assert.assertNull(obj);
-        obj = JSONFunctions.get(json,"///test///test2");
+        obj = JSONFunctions.get(json, "///test///test2");
         Assert.assertNotNull(obj);
-        Assert.assertEquals(obj,1);
-        
+        Assert.assertEquals(obj, 1);
+        json = new JSONObject();
+        json.put("test", new JSONArray().put(0).put(1).put(3));
+        Assert.assertEquals(JSONFunctions.get(json, "test[0]"), 0);
+        Assert.assertEquals(JSONFunctions.get(json, "test[1]"), 1);
+        Assert.assertEquals(JSONFunctions.get(json, "test[2]"), 3);
+        JSONObject test2 = new JSONObject().put("test2", json);
+        Assert.assertEquals(JSONFunctions.get(test2, "test2/test[0]"), 0);
+        Assert.assertEquals(JSONFunctions.get(test2, "test2/test[1]"), 1);
+        Assert.assertEquals(JSONFunctions.get(test2, "test2/test[2]"), 3);
+        JSONObject test3 = new JSONObject().put("test3", new JSONArray().put(json));
+        Assert.assertEquals(JSONFunctions.get(test3, "test3[0]/test[0]"), 0);
+        Assert.assertEquals(JSONFunctions.get(test3, "test3[0]/test[1]"), 1);
+        Assert.assertEquals(JSONFunctions.get(test3, "test3[0]/test[2]"), 3);
+        JSONObject weather = new JSONObject("{\"coord\":{\"lon\":8.8078,\"lat\":53.0752},\"weather\":[{\"id\":804,\"main\":\"Clouds\",\"description\":\"overcast clouds\",\"icon\":\"04d\"}],\"base\":\"stations\",\"main\":{\"temp\":19.44,\"feels_like\":19.55,\"temp_min\":17.12,\"temp_max\":19.89,\"pressure\":1015,\"humidity\":81},\"visibility\":10000,\"wind\":{\"speed\":2.06,\"deg\":250},\"clouds\":{\"all\":100},\"dt\":1656330604,\"sys\":{\"type\":1,\"id\":1281,\"country\":\"DE\",\"sunrise\":1656298811,\"sunset\":1656359711},\"timezone\":7200,\"id\":2944388,\"name\":\"Bremen\",\"cod\":200}");
+        Assert.assertEquals(JSONFunctions.get(weather, "coord/lon"), 8.8078);
+        Assert.assertEquals(JSONFunctions.get(weather, "weather[0]/id"), 804);
+        Assert.assertEquals(JSONFunctions.get(weather, "clouds/all"), 100);
     }
 }
 
